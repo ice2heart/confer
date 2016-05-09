8106468369c303b5ef1ac04e5bcb8328df0ab09f
ice2heart <ice2heart@gmail.com>
2016-05-10 00:04:07 +0500

    init

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+    "encoding/json"
+    "log"
+    "fmt"
+    "os"
+)
+
+
+func main() {
+	fmt.Println("Hello, 世界")
+  dec := json.NewDecoder(os.Stdin)
+  enc := json.NewEncoder(os.Stdout)
+  for {
+      var v map[string]interface{}
+      if err := dec.Decode(&v); err != nil {
+          log.Println(err)
+          return
+      }
+      for k := range v {
+          if k != "Name" {
+              delete(v, k)
+          }
+      }
+      if err := enc.Encode(&v); err != nil {
+          log.Println(err)
+      }
+  }
+}
